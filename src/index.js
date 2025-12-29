b8f420cab3c57825fe52eff62bd58d94a41eaae7
Tarun Karn <tarunkarnofficial@gmail.com>
2025-12-29 22:23:52 +0530

    util setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 import connectDB from "./db/index.js";
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at env ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed !!", err);
+  });
 
 // import express from "express";
 // const app = express()(async () => {
